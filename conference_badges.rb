d628419457d591f98e0929b7c30655ff973f4bf7
Veronica Lopez <nicaa0695@gmail.com>
2019-07-17 18:21:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,10 @@
 def badge_maker(name)
     "Hello, my name is #{name}."
 end
+def batch_badge_creator(array_of_names)
+  batch_array = []
+  array_of_names.each do |name|
+    batch_array << badge_maker(name)
+  end
+  batch_array
+end
